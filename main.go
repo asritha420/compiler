581b94dbcf73d01a62c86de0dfae31a324b0996e
Asritha <asrithab2022@gmail.com>
2024-11-25 13:43:27 -0500

    updating

--- main.go
+++ main.go
@@ -1,159 +1,29 @@
 package main
 
 import (
-	// "fmt"
-	// "log"
-
-	// "asritha.dev/compiler/pkg/parsergen"
-	"asritha.dev/compiler/pkg/scannergen"
-	// "asritha.dev/compiler/tests"
-	// "google.golang.org/genproto/googleapis/home/graph/v1"
-	// "google.golang.org/grpc"
+	"asritha.dev/compiler/pkg/parsergen"
+	"fmt"
 )
 
 func main() {
-	TestFirstAndFollowSets()
-	// TestRegexParser()
-}
-func TestFirstAndFollowSets() {
-	// 	g9 := tests.TestG9
 
-	// 	/*
-	// 		80 = P
-	// 		69 = E
-	// 		88 = X (E')
-	// 		84 = T
-	// 		85 = U (T')
-	// 		70 = F
-	// 	*/
+	test, _ := parsergen.ConvertProduction("EE'T\" \\\"7\\\\\"", []string{"E", "T", "E'"})
 
-	// 	//TODO: is it ok if these are maps, or will this mess up some order in the code?
-	// 	for nt, set := range g9.FirstSet {
-	// 		fmt.Printf("FIRST SET %c: ", nt)
-	// 		for _, terminal := range set {
-	// 			fmt.Printf("%c", terminal)
-	// 		}
-	// 		fmt.Println()
-	// 	}
-
-	// 	for nt, set := range g9.FollowSet {
-	// 		fmt.Printf("FOLLOW SET %c: ", nt)
-	// 		for _, terminal := range set {
-	// 			fmt.Printf("%c", terminal)
-	// 		}
-	// 		fmt.Println()
-	// 	}
-	// }
-
-<<<<<<< HEAD
-func TestRegexParser() {
-	rules := []parsergen.Rule{
-		parsergen.Rule{
-			NonTerminal: "P",
-			Productions: []string{},
-		},
+	// \"7\\
+	for _, t := range test {
+		fmt.Printf("%c", t)
 	}
-	test := parsergen.NewLL1Grammar()
-=======
-	// func TestRegexParser() {
-	// 	//TODO: this should be using the initializer function
-	// 	regexGrammar := parsergen.NewLL1Grammar(
-	// 		map[byte][]string{
-	// 			'P': {"E"},
-	// 			'E': {"TX"},
-	// 			'X': {"|TX", parsergen.Epsilon},
-	// 			'T': {"FY"},
-	// 			'Y': {"FY", parsergen.Epsilon},
-	// 			'F': {"GM"},
-	// 			'M': {"*M", parsergen.Epsilon},
-	// 			'G': {"(E)", parsergen.ValidChar, parsergen.ValidInt},
-	// 		},
-	// 		[]byte{'|', '*', '(', ')'},
-	// 		[]byte{'P', 'E', 'X', 'T', 'Y', 'F', 'M', 'G'},
-	// 	)
-
-	// 	regexTests := []string{"a(B|c)*", "cat|cow", "a(b|a|0|d)*a", "a**", "(abb)*", "a(cow|cat)*"}
-
-	// 	for _, rt := range regexTests {
-	// 		rParser := scannergen.NewRegexParser(regexGrammar, rt)
-	// 		ast, err := rParser.Parse()
-	// 		if err != nil {
-	// 			fmt.Printf("❌ PARSING ERROR: %v\n", err)
-	// 		}
-	// 		fmt.Printf("%+v: \n", ast)
-
-	// 		if val, ok := ast.(scannergen.ASTPrinter); ok {
-	// 			fmt.Printf("%v \n \n", val.PrintNode(""))
-	// 		}
-	// 	}
-
-	// 	testRParser := scannergen.NewRegexParser(regexGrammar, "a(cow|cat)*")
-	// 	ast, err := testRParser.Parse()
-	// 	if err != nil {
-	// 		fmt.Printf("❌ PARSING ERROR: %v\n", err)
-	// 	}
-
-	// 	graph, _, _, err := scannergen.ConvertRegexToNfa(ast)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	graph := &scannergen.Concatenation{
-		Left: &scannergen.Const{Value: 'a'},
-		Right: &scannergen.KleeneStar{
-			Left: &scannergen.Alternation{
-			Left:  &scannergen.Const{Value: '❌'},
-			Right: &scannergen.Const{Value: 'w'},
-		},
-	},
-	}
-	NFA, _, _, _ := scannergen.ConvertRegexToNfa(graph)
-	DFA, _ := scannergen.ConvertNFAtoDFA(NFA)
-	println(scannergen.MakeMermaid(DFA))
->>>>>>> 9d4db2b6c573136beb0a38e0c5de13658bd7d7ac
 }
 
-//func TestRegexParser() {
-//	//TODO: this should be using the initializer function
-//	regexGrammar := parsergen.NewLL1Grammar(
-//		map[byte][]string{
-//			'P': {"E"},
-//			'E': {"TX"},
-//			'X': {"\"|\"TX", parsergen.Epsilon},
-//			'T': {"FY"},
-//			'Y': {"FY", parsergen.Epsilon},
-//			'F': {"GM"},
-//			'M': {"*M", parsergen.Epsilon},
-//			'G': {"(E)", parsergen.ValidChar, parsergen.ValidInt},
-//		},
-//		[]byte{'|', '*', '(', ')'},
-//		[]byte{'P', 'E', 'X', 'T', 'Y', 'F', 'M', 'G'},
-//	)
+//func RegexParser() {
+//	production := parsergen.NewRule("P", []string{"E"})
+//	expression := parsergen.NewRule("E", []string{"TE'"})
+//	expressionPrime := parsergen.NewRule("E'", []string{"\"|\"TE'", ""})
+//	term := parsergen.NewRule("T", []string{"FT'"})
+//	termPrime := parsergen.NewRule("T'", []string{"FT'", ""})
+//	factor := parsergen.NewRule("F", []string{"GF'"})
+//	factorPrime := parsergen.NewRule("F'", []string{"\"*\"F'", ""})
+//	group := parsergen.NewRule("G", []string{"\"(\"E\")\"", "v"})
 //
-//	regexTests := []string{"a(B|c)*", "cat|cow", "a(b|a|0|d)*a", "a**", "(abb)*", "a(cow|cat)*"}
-//
-//	for _, rt := range regexTests {
-//		rParser := scannergen.NewRegexParser(regexGrammar, rt)
-//		ast, err := rParser.Parse()
-//		if err != nil {
-//			fmt.Printf("❌ PARSING ERROR: %v\n", err)
-//		}
-//		fmt.Printf("%+v: \n", ast)
-//
-//		if val, ok := ast.(scannergen.ASTPrinter); ok {
-//			fmt.Printf("%v \n \n", val.PrintNode(""))
-//		}
-//	}
-//
-//	testRParser := scannergen.NewRegexParser(regexGrammar, "a(cow|cat)*")
-//	ast, err := testRParser.Parse()
-//	if err != nil {
-//		fmt.Printf("❌ PARSING ERROR: %v\n", err)
-//	}
-//
-//	graph, _, _, err := scannergen.ConvertRegexToNfa(ast)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//	DFA, _ := scannergen.ConvertNFAtoDFA(graph)
-//	println(scannergen.MakeMermaid(DFA))
+//	parsergen.NewGrammar([]*parsergen.Rule{production, expression, expressionPrime, term, termPrime, factor, factorPrime, group})
 //}
--- pkg/parsergen/grammarRule.go
+++ pkg/parsergen/grammarRule.go
@@ -1,17 +1,111 @@
 package parsergen
 
+import "fmt"
+
 type Rule struct {
 	NonTerminal string
-	Productions []string
+	Productions [][]RuleToken
 	firstSet    []string
 	followSet   []string
+	terminals   []rune
 }
 
-func NewRule(nT string, productions []string) (*Rule, error) {
-	//TODO: implement error handling if inaccurate rule
-	r := &Rule{
-		NonTerminal: nT,
-		Productions: productions,
+type RuleToken interface {
+}
+
+type Terminal struct {
+	value rune
+}
+
+type NonTerminal struct {
+	value string
+}
+
+//
+//func NewRule(nT string, productions []string) *Rule {
+//	return &Rule{
+//		NonTerminal: nT,
+//		Productions: productions,
+//	}
+//}
+
+//func NewRules(rules string) []*Rule {
+/*
+	for each line
+
+	lh of arrow: becomes nt string
+	rh of arrow: is prodcution, will convert into tokens
+
+*/
+//}
+
+func ConvertProduction(p string, validNT []string) ([]RuleToken, error) {
+	rules := make([]RuleToken, 0)
+	isTerminal := false
+	ntStart := -1
+	var ntString string
+	isEscaped := false
+	for i, ch := range p {
+		if ch == '"' {
+			if isEscaped && isTerminal {
+				isEscaped = false
+			} else {
+				isTerminal = !isTerminal
+				continue
+			}
+		}
+		if isTerminal {
+			if ch == '\\' {
+				if isEscaped {
+					isEscaped = false
+				} else {
+					isEscaped = true
+					continue
+				}
+			}
+			rules = append(rules, Terminal{value: ch})
+		} else {
+			if ntStart == -1 {
+				ntStart = i
+				ntString = p[ntStart:]
+			}
+
+			if !prodLookAhead(ntString, i-ntStart+1, validNT) {
+				// found new NT
+				ntString = ntString[:i-ntStart+1]
+				if err := matchesNT(ntString, validNT); err != nil {
+					return nil, err
+				}
+				rules = append(rules, NonTerminal{value: ntString})
+				ntStart = -1
+			}
+		}
 	}
-	return r, nil
+	return rules, nil
+}
+
+func prodLookAhead(p string, index int, validNT []string) bool {
+	for i := 0; i < len(validNT); i++ {
+		if len(validNT[i]) > index && validNT[i][index] == p[index] {
+			return true
+		}
+	}
+	return false
+}
+
+func matchesNT(p string, validNT []string) error {
+	matched := false
+	for _, nt := range validNT {
+		if p == nt {
+			if matched {
+				return fmt.Errorf("multiple tokens matched %s", p)
+			}
+			matched = true
+		}
+	}
+	if !matched {
+		return fmt.Errorf("token %s not found", p)
+	} else {
+		return nil
+	}
 }
