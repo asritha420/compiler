414e53961afae533a450d2b75d67c62fad2d0bc8
Asritha <asrithab2022@gmail.com>
2025-01-07 19:23:46 -0500

    getClosure works

--- pkg/asritha_tmp/parsing/lr0_automaton.go
+++ pkg/asritha_tmp/parsing/lr0_automaton.go
@@ -14,49 +14,45 @@ type State struct {
 
 // NewLR0Automaton creates a LR0Automaton, which represents all possible rules currently under consideration by a shit-reduce parser
 func (g *LRGrammar) NewLR0Automaton() *State {
-	kernel := &StateItem{
-		Rule:              g.rules[0],
-		dotIsToTheRightOf: 0,
-	}
-	state0 := NewState(kernel)
+	state0 := g.NewState()
 	return state0
 }
 
-func NewState(kernel *StateItem) *State {
+func (g *LRGrammar) NewState() *State {
 	s := &State{
-		items: []*StateItem{kernel},
+		items: make([]*StateItem, 0),
 	}
-	s.getClosure()
+	//TODO: have comment saying g.nonTerminals[0] is kernel? -> also this function should be reusable ??
+	s.getClosureFor(g, g.nonTerminals[0])
 	return s
 }
 
-// TODO: shouldn't pass in grammar
-func (s *State) getClosure(g *LRGrammar) {
-	for _, item := range s.items {
-		// if dot is to the right of a non-terminal
-		symbol := item.Rule.production[item.dotIsToTheRightOf+1]
-		if isValidNonTerminal(symbol) {
-			_ = getClosureFor(g, symbol)
-		}
-	}
-}
-
-func getClosureFor(g *LRGrammar, nT rune) []*StateItem {
-	startStateItems := make([]*StateItem, 0)
-	// add all rules in the grammar that have symbol as a non-terminal
-	for _, rule := range g.rules {
-		if rule.nonTerminal == nT {
-			startStateItems = append(startStateItems, &StateItem{
-				Rule:              rule,
-				dotIsToTheRightOf: -1,
-			})
-			startStateItems = append(startStateItems, getClosureFor(g, nT)...)
-		}
-	}
-	return startStateItems
-}
-
-// should be a method on grammar or symbol type?
-//func isValidNonTerminal(symbol rune) bool {
+// TODO: have separate fields for kernelItems and nonKernelItems in state?
+// TODO: don't like how I am passing in grammar
 //
-//}
+//	func (s *State) getClosure(g *LRGrammar) {
+//		s.items = s.getClosureFor(g, g.nonTerminals[0])
+//	} //
+//
+// start with P
+func (s *State) getClosureFor(g *LRGrammar, nT rune) {
+	// TODO: rename this nonKernelItems?
+	//startStateItems := make([]*StateItem, 0)
+	// add all rules in the grammar that have symbol as a non-terminal
+	matchingRules := g.getRulesForNT(nT)
+	for _, rule := range matchingRules {
+		item := &StateItem{
+			Rule:              rule,
+			dotIsToTheRightOf: 0,
+		}
+
+		s.items = append(s.items, item)
+
+		// if dot is to the right of a non-terminal
+		symbol := item.Rule.production[item.dotIsToTheRightOf]
+		if g.isValidNonTerminal(symbol) && symbol != nT {
+			//startStateItems = append(startStateItems, s.getClosureFor(g, symbol)...)
+			s.getClosureFor(g, symbol)
+		}
+	}
+}
--- pkg/asritha_tmp/parsing/lr_automaton_test.go
+++ pkg/asritha_tmp/parsing/lr_automaton_test.go
@@ -1,11 +1,60 @@
 package aparsing
 
+import (
+	"reflect"
+	"testing"
+)
+
 var (
+	pRule     = NewRule('P', "E")
+	eRule     = NewRule('E', "E+T")
+	eRule2    = NewRule('E', "T")
+	tRule     = NewRule('T', "i(E)")
+	tRule2    = NewRule('T', "i")
 	grammar10 = LRGrammar{
 		rules: []*Rule{
+			pRule,
+			eRule,
+			eRule2,
+			tRule,
+			tRule2,
+		},
+		nonTerminals: []rune{'P', 'E', 'T'},
+		terminals:    []rune{'+', 'i', '(', ')'},
+	}
+)
+
+func TestNewState(t *testing.T) {
+	shouldEqual := &State{
+		items: []*StateItem{
 			{
-				nonTerminal: 'P',
+				Rule:              pRule,
+				dotIsToTheRightOf: 0,
+			},
+			{
+				Rule:              eRule,
+				dotIsToTheRightOf: 0,
+			},
+			{
+				Rule:              eRule2,
+				dotIsToTheRightOf: 0,
+			},
+			{
+				Rule:              tRule,
+				dotIsToTheRightOf: 0,
+			},
+			{
+				Rule:              tRule2,
+				dotIsToTheRightOf: 0,
 			},
 		},
 	}
-)
+
+	kernel := &StateItem{
+		Rule:              pRule,
+		dotIsToTheRightOf: 0,
+	}
+	if !reflect.DeepEqual(shouldEqual, grammar10.NewState(kernel)) {
+		t.Errorf("FAILED")
+	}
+}
