35d3d6bbc9d51b0616c7de4a4859c92d00ccbf8c
Asritha <asrithab2022@gmail.com>
2025-01-09 12:03:39 -0500

    changing name in my shit

---
+++ pkg/asritha_tmp/parsergen/lr0_automaton.go
@@ -0,0 +1,121 @@
+package aparsergen
+
+// TODO: create newStateItem() ?? -> when to have Newxxx() and when to not?
+type StateItem struct {
+	*Rule
+	// dot indicates the parser's current position in the rule
+	dotIsToTheRightOf int //make required somehow, but it wont matter bc a user won't initalize this?
+}
+
+// TODO: call this DocGenerator?
+type MermaidPrinter interface {
+	printInMermaid() string
+}
+
+// TODO: have field tags for kernel vs non-kernel items ??
+type State struct {
+	items       []*StateItem
+	transitions map[rune]*State
+}
+
+type LR0Automaton struct{}
+
+func (lra *LR0Automaton) printInMermaid() string {
+	return ""
+}
+
+func (s *State) printInMermaid(counter int) string {
+	var mermaidString string // TODO: rename
+
+	for _, item := range s.items {
+		var itemProduction string
+		// TODO: simpler way to do this??
+		for index, symbol := range item.production {
+			if item.dotIsToTheRightOf == index {
+				itemProduction = ". "
+			}
+			itemProduction += string(symbol) + " "
+		}
+		mermaidString = mermaidString + string(item.nonTerminal) + "= " + itemProduction + "\n"
+	}
+
+	mermaidString = "state" + string(counter) + "[\"`\n\t**State" + string(counter) + "[\"`]"
+
+	return mermaidString
+}
+
+// TODO: figure out when to "reach accepting"
+// NewLR0Automaton creates a LR0Automaton, which represents all possible rules currently under consideration by a shit-reduce parser
+func (g *LRGrammar) NewLR0Automaton() *State {
+	state0Kernel := []*StateItem{
+		{
+			Rule:              g.rules[0],
+			dotIsToTheRightOf: 0,
+		},
+	}
+	state0 := g.NewState(state0Kernel)
+	return state0
+}
+
+// TODO: create mermaid graph printer
+
+// TODO: if having same state, should point to same state instead of redoing it
+func (g *LRGrammar) NewState(kernel []*StateItem) *State {
+	s := &State{
+		items: make([]*StateItem, 0),
+	}
+	for _, kernelItem := range kernel {
+		s.getClosure(g, kernelItem)
+	}
+
+	s.CreateTransitions(g)
+	return s
+}
+
+// create transititions for each symbol to the right of dot
+func (s *State) CreateTransitions(g *LRGrammar) {
+	// get list of all possible transitions
+	transitionKernels := make(map[rune][]*StateItem)
+	for _, item := range s.items {
+		if item.dotIsToTheRightOf == len(item.production) {
+			continue
+		}
+		transitionSymbol := item.production[item.dotIsToTheRightOf]
+
+		newItem := &StateItem{
+			Rule:              item.Rule,
+			dotIsToTheRightOf: item.dotIsToTheRightOf + 1,
+		}
+
+		if existing, ok := transitionKernels[transitionSymbol]; ok {
+			existing = append(existing, newItem)
+		} else {
+			transitionKernels[transitionSymbol] = []*StateItem{newItem}
+		}
+	}
+
+	for transition, kernel := range transitionKernels {
+		s.transitions[transition] = g.NewState(kernel)
+	}
+}
+
+// TODO: add more edge cases in test
+// TODO: handle edge case: E -> T + E; T -> E
+// TODO: have separate fields for kernelItems and nonKernelItems in state?
+func (s *State) getClosure(g *LRGrammar, stateItem *StateItem) {
+	// add all rules in the grammar that have symbol as a non-terminal
+	symbol := stateItem.production[stateItem.dotIsToTheRightOf]
+	if g.isValidNonTerminal(symbol) && symbol != stateItem.nonTerminal {
+		matchingRules := g.getRulesForNT(symbol) //TODO: have consistent naming everywhere for nT/nonTerminal
+		for _, rule := range matchingRules {
+			item := &StateItem{
+				Rule:              rule,
+				dotIsToTheRightOf: 0,
+			}
+
+			s.items = append(s.items, item)
+
+			s.getClosure(g, item)
+		}
+	}
+}
---
+++ pkg/asritha_tmp/parsergen/notes/lr_grammars.md
@@ -0,0 +1,64 @@
+# LR Grammars
+
+### LR(0) Automaton 
+
+- **LR(1) grammars**: grammars that can be parsed via shift-reduce w/ single token lookahead 
+  - LR
+      - L: left-to-right scanning on input
+      - R: produce rightmost derivation 
+  - can accommodate left recursion
+  - grammar must be non-ambiguous 
+  - cannot have shift-reduce or reduce-reduce conflicts
+
+
+- Shift-Reduce Parsing: 
+  - "bottom up" parsing strategy: start with stream of tokens, look for rules that can be applied to reduce sentential forms into non-terminals
+    - for parsing to be successful, the sequence of reductions must lead to start symbol
+  - *shift action*: consume one token from input stream, push it onto the stack
+  - *reduce action*: apply one from of the form $A \rightarrow a$ from grammar, so replaces sentential form $a$ on stack with the corresponding non-terminal
+
+
+- *LR(0) automaton*: represents choices available at any step of bottom up parsing  
+  - also called canonical collection or compact finite state machine of grammar
+  - algorithm:
+    - Create State 0
+      - Get Kernel of State 0 
+      - Get Closure of State 0: for each item in the state with a non-terminal X immediately to the right of the dot, add all rules in the grammar that have X as a non-terminal
+      - Create transitions for each of the symbols to the right of the dot 
+    - For each transition, create a new state containing the matching items with dot moved one position to the right
+    - Continue procedure until no new items can be added
+    - Key points: 
+      - kernel items: start items + items where dot is not at the beginning 
+      - non-kernel items: items introduced by the closure operation 
+  - state w/ item w/ dot at end rule: possible reduction 
+  - transition on a terminal that moves dot one position to the right: possible shift
+  - conflicts: 
+    - *shift-reduce conflict*: choice between shift action and reduce action in the same state 
+    - *reduce-reduce conflict*: two distinct rules have been matched 
+
+### SLR Parsing 
+
+- **Simple LR (SLR)** parsing: use FOLLOW sets to resolve conflicts in the LR(0) automaton 
+
+--- 
+
+Given  $G_{10}$: 
+- $P \rightarrow E$ 
+- $E \rightarrow E + T$ 
+- $E \rightarrow T$ 
+- $E \rightarrow id(E)$
+- $T \rightarrow id$ 
+
+
+**1) Calculate FIRST and FOLLOW sets**
+
+FIRST set:
+- $P$: $id$
+- $E$: $id$
+- $T$: $id$
+
+FOLLOW set: 
+
+- $P$: $\$$
+- $E$: $\$$, $+$, $)$
+- $T$: $\$$
