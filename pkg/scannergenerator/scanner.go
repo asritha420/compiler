b5d58aef60b58e32d533af43b474f93c1cef2a59
Asritha <asrithab2022@gmail.com>
2025-01-11 23:24:58 -0500

    update

--- pkg/scannergenerator/scanner.go
+++ pkg/scannergenerator/scanner.go
@@ -2,43 +2,58 @@ package scannergenerator
 
 import (
 	"regexp"
-	"strings"
 )
 
 type Scanner struct {
-	tokenSpec map[TokenType]regexp.Regexp
+	tokenSpec []TokenInfo // in order of token priority
+}
+
+type TokenInfo struct {
+	TokenType
+	regexp      regexp.Regexp
+	regexString string
 }
 
 func (s *Scanner) Scan(code string) ([]Token, error) {
 
 	tokenStream := make([]Token, 0)
+	currToken := &Token{}
+	var currWord string
 
-	codeWords := strings.Split(code, " ")
-
-	for _, word := range codeWords {
-		for tokenType, regex := range s.tokenSpec {
-			if regex.MatchString(word) {
-				tokenStream = append(tokenStream, Token{
-					TokenType: tokenType,
-					Lexeme:    word,
-				})
+	matchesNone := func(currWord string) bool {
+		for _, ts := range s.tokenSpec {
+			if ts.regexp.MatchString(currWord) {
+				currToken.TokenType = ts.TokenType
+				currToken.Lexeme = currWord
+				return false
 			}
+		}
+		return true
+	}
+
+	for _, character := range code {
+		currWord += string(character)
+		if matchesNone(currWord) {
+			tokenStream = append(tokenStream, *currToken)
+			currWord = ""
 		}
 	}
 
 	return tokenStream, nil
 }
 
-func NewScanner(tokenSpec map[TokenType]string) (*Scanner, error) {
+func NewScanner(tokenSpec []TokenInfo) (*Scanner, error) {
 	s := &Scanner{
-		tokenSpec: make(map[TokenType]regexp.Regexp),
+		tokenSpec: make([]TokenInfo, 0),
 	}
-	for tokenType, regexString := range tokenSpec {
-		regex, err := regexp.Compile(regexString)
+
+	for _, tokenInfo := range s.tokenSpec {
+		regex, err := regexp.Compile(tokenInfo.regexString)
 		if err != nil {
 			return nil, err
 		}
-		s.tokenSpec[tokenType] = *regex
+		tokenInfo.regexp = *regex
 	}
+
 	return s, nil
 }
